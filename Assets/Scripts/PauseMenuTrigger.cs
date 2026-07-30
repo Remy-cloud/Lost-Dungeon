34a6dda8d09edc3101948b784b68dc73a584597c
Remy-cloud <r.intwali@alustudent.com>
2026-07-30 13:08:06 +0200

    changed the way my escape key wasnt working due to overlays with other UI

--- Assets/Scripts/PauseMenuTrigger.cs
+++ Assets/Scripts/PauseMenuTrigger.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class PauseMenuTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenuCanvas;
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject levelSelectPanel;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             mainMenuCanvas.SetActive(true);
+            mainMenuPanel.SetActive(true);
+            levelSelectPanel.SetActive(false);
             Time.timeScale = 0f;
         }
     }
